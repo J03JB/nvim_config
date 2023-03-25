f76592cdeb216dbc9318adb584c6eb77e97c8811
Joe B <>
2023-03-25 08:36:14 +0000

    macOS: added settings for Neovide

--- lua/freesec/settings.lua
+++ lua/freesec/settings.lua
@@ -112,5 +112,22 @@ else
 	vim.g.python3_host_prog = "/usr/bin/python"
 end
 
+-----------------------------------------------------------------------------------------
+-- Neovide settings for macOS
+-----------------------------------------------------------------------------------------
+if vim.g.neovide then
+	vim.g.neovide_confirm_quit = true
+	vim.g.neovide_cursor_animate_command_line = false
+	vim.g.neovide_input_macos_alt_is_meta = true
+	vim.g.neovide_input_use_logo = 1
+	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+	-- set dir to $HOME when Neovide opens.
+	vim.api.nvim_create_autocmd({ "VimEnter" }, { pattern = "*", command = ":cd $HOME " })
+end
+
 DATA_PATH = vim.fn.stdpath("data")
 CACHE_PATH = vim.fn.stdpath("cache")
