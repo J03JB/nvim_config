a6bc10ef5b775abb41b45a95b9b628e3502d289c
Joe B <>
2023-04-10 11:34:21 +0100

    added harpoon to telescope. ordered telescope key mappings

--- lua/freesec/plugin_config/telescope.lua
+++ lua/freesec/plugin_config/telescope.lua
@@ -26,7 +26,7 @@ require("telescope").setup({
 			override_generic_sorter = true, -- override the generic sorter
 			override_file_sorter = true, -- override the file sorter
 		},
-		packer = {
+		lazy = {
 			theme = "ivy",
 			layout_config = {
 				height = 0.5,
@@ -103,4 +103,6 @@ require("telescope").setup({
 require("telescope").load_extension("ui-select")
 require("telescope").load_extension("fzf")
 require("telescope").load_extension("file_browser")
+require("telescope").load_extension("harpoon")
+require("telescope").load_extension("lazy")
 -- require("telescope").load_extension("possession")
