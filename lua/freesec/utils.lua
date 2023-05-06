90a42368511701bef9722154fd0ad13d5cc0654d
Joe B <>
2023-05-06 18:06:16 +0100

    remapped K to work with crates.nvim

--- lua/freesec/utils.lua
+++ lua/freesec/utils.lua
@@ -93,4 +93,19 @@ function M.create_augroups(definitions)
 	end
 end
 
+local function show_documentation()
+	local filetype = vim.bo.filetype
+	if vim.tbl_contains({ "vim", "help" }, filetype) then
+		vim.cmd("h " .. vim.fn.expand("<cword>"))
+	elseif vim.tbl_contains({ "man" }, filetype) then
+		vim.cmd("Man " .. vim.fn.expand("<cword>"))
+	elseif vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+		require("crates").show_popup()
+	else
+		vim.lsp.buf.hover()
+	end
+end
+
+vim.keymap.set("n", "K", show_documentation, { silent = true })
+
 return M
