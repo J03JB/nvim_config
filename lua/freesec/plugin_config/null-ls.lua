07c96d005d2f3d9c209c4661ceffeb671c7bdaa2
Frees3c <joeyjordanbarnes@hotmail.com>
2023-05-10 22:50:35 +0100

    removed format on save

--- lua/freesec/plugin_config/null-ls.lua
+++ lua/freesec/plugin_config/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls = require("null-ls")
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 require("null-ls").setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
@@ -12,25 +12,25 @@ require("null-ls").setup({
 		null_ls.builtins.formatting.shfmt,
 		null_ls.builtins.code_actions.gitsigns,
 	},
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
+	-- on_attach = function(client, bufnr)
+	-- 	if client.supports_method("textDocument/formatting") then
+	-- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+	-- 		vim.api.nvim_create_autocmd("BufWritePre", {
+	-- 			group = augroup,
+	-- 			buffer = bufnr,
+	-- 			callback = function()
+	-- 				vim.lsp.buf.format({ bufnr = bufnr })
+	-- 			end,
+	-- 		})
+	-- 	end
+	-- end,
 })
 
-local lsp_formatting = function(bufnr)
-	vim.lsp.buf.format({
-		filter = function(client)
-			return client.name == "null-ls"
-		end,
-		bufnr = bufnr,
-	})
-end
+-- local lsp_formatting = function(bufnr)
+-- 	vim.lsp.buf.format({
+-- 		filter = function(client)
+-- 			return client.name == "null-ls"
+-- 		end,
+-- 		bufnr = bufnr,
+-- 	})
+-- end
