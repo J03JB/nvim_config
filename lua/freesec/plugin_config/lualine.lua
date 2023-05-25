9c4c9554ab1b71a55bdef0619cf7f2392321eb20
Frees3c <>
2023-05-25 23:17:34 +0100

    modified theme for  lualine

--- lua/freesec/plugin_config/lualine.lua
+++ lua/freesec/plugin_config/lualine.lua
@@ -1,16 +1,58 @@
+local colors = {
+	bg = "#282c34",
+	fg = "#abb2bf",
+	yellow = "#e0af68",
+	cyan = "#56b6c2",
+	darkblue = "#081633",
+	green = "#98c379",
+	orange = "#d19a66",
+	violet = "#a9a1e1",
+	magenta = "#c678dd",
+	blue = "#61afef",
+	red = "#e86671",
+}
+
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = "auto",
+		-- theme = "auto",
+		theme = {
+			normal = {
+				a = { fg = colors.fg, bg = colors.bg },
+				b = { fg = colors.yellow, bg = colors.bg },
+				c = { fg = colors.fg, bg = colors.bg },
+			},
+			insert = { a = { fg = colors.blue, bg = colors.bg }, b = { fg = colors.yellow, bg = colors.bg } },
+			visual = { a = { fg = colors.magenta, bg = colors.bg }, b = { fg = colors.yellow, bg = colors.bg } },
+			command = { a = { fg = colors.orange, bg = colors.bg }, b = { fg = colors.yellow, bg = colors.bg } },
+			replace = { a = { fg = colors.fg, bg = colors.bg }, b = { fg = colors.yellow, bg = colors.bg } },
+
+			inactive = {
+				a = { bg = colors.fg, fg = colors.bg },
+				b = { bg = colors.yellow, fg = colors.bg },
+				c = { bg = colors.fg, fg = colors.bg },
+			},
+		},
+		-- section_separators = { left = "", right = "" },
+		-- component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
-		component_separators = { left = "", right = "" },
+		ciiiiiiomponent_separators = { left = "", right = "" },
 	},
 	sections = {
 		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff" },
+		lualine_b = {
+			"branch",
+			{
+				"diff",
+				diff_color = {
+					added = { fg = "#99c794" },
+					modified = { fg = "#5bb7b8" },
+					removed = { fg = "#ec5f67" },
+				},
+			},
+		},
 		lualine_c = { "filename" },
 		lualine_x = {
-			"searchcount",
 			{ "diagnostics", sources = { "nvim_diagnostic" } },
 			"filetype",
 			"encoding",
