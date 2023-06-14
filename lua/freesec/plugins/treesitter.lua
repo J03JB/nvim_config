8f7d2d4916a940f22bafe09db25716cdd118d680
Frees3c <joeyjordanbarnes@hotmail.com>
2023-06-14 19:14:22 +0100

    added ts-rainbow back to treesitter

--- lua/freesec/plugins/treesitter.lua
+++ lua/freesec/plugins/treesitter.lua
@@ -1,34 +1,42 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "rust", "toml", "c", "lua", "vim", "vimdoc", "javascript", "typescript", "python" },
-	sync_install = false,
-	auto_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
+    ensure_installed = { "rust", "toml", "c", "lua", "vim", "vimdoc", "javascript", "typescript", "python" },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
     indent = {
         enable = true,
     },
-	refactor = {
-		highlight_definitions = {
-			enable = true,
-		},
-	},
-	autotag = {
-		enable = true,
-	},
-	-- context_commentstring = {
-	-- 	enable = true,
-	-- },
-	textobjects = {
-		select = {
-			enable = true,
-			keymaps = {
-				["af"] = "@function.outer",
-				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ic"] = "@class.inner",
-			},
-		},
-	},
+    refactor = {
+        highlight_definitions = {
+            enable = true,
+        },
+    },
+    autotag = {
+        enable = true,
+    },
+    context_commentstring = {
+        enable = true,
+    },
+    rainbow = {
+        enable = true,
+        disable = { 'zig', 'jsx', 'cpp' },
+        -- Which query to use for finding delimiters
+        query = 'rainbow-parens',
+        -- Highlight the entire buffer all at once
+        strategy = require('ts-rainbow').strategy.global,
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+            },
+        },
+    },
 })
