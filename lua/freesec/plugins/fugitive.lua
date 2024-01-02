863b4df6b8a44fc541241a1b7a3b75bf709b00b3
Frees3c <joeyjordanbarnes@hotmail.com>
2024-01-02 21:37:32 +0000

    refactor

--- lua/freesec/plugins/fugitive.lua
+++ lua/freesec/plugins/fugitive.lua
@@ -1,28 +1,33 @@
--- TODO: modify this. possibly add to autocmds.lua
-local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+local gitfug = vim.api.nvim_create_augroup("gitfug", {} )
 
 local autocmd = vim.api.nvim_create_autocmd
-autocmd("BufWinEnter", {
-	group = ThePrimeagen_Fugitive,
-	pattern = "*",
-	callback = function()
-		if vim.bo.ft ~= "fugitive" then
-			return
-		end
+    autocmd("BufWinEnter", {
+        group = gitfug,
+        pattern = "*",
+        callback = function()
+                if vim.bo.ft ~= "fugitive" then
+                    return
+                end
 
-		local bufnr = vim.api.nvim_get_current_buf()
-		local opts = { buffer = bufnr, remap = false }
-		vim.keymap.set("n", "<leader>p", function()
-			vim.cmd.Git("push")
-		end, opts)
+                local bufnr = vim.api.nvim_get_current_buf()
+                local opts = { buffer = bufnr, remap = false }
+                vim.keymap.set("n", "<leader>p", function()
+                    vim.cmd.Git("push")
+                end, opts)
 
-		-- rebase always
-		vim.keymap.set("n", "<leader>P", function()
-			vim.cmd.Git({ "pull", "--rebase" })
-		end, opts)
+                vim.keymap.set("n", "<leader>c", function()
+                    vim.cmd.Git("commit")
+                end, opts)
 
-		-- NOTE: It allows me to easily set the branch i am pushing and any tracking
-		-- needed if i did not set the branch up correctly
-		vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
-	end,
-})
+                -- rebase always
+                vim.keymap.set("n", "<leader>P", function()
+                    vim.cmd.Git({ "pull", "--rebase" })
+                end, opts)
+
+                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+                -- needed if i did not set the branch up correctly
+                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+
+
+            end,
+        })
