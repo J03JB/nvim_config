114c6674f1646a6433520a3261bf466a15fc8bf3
Frees3c <joeyjordanbarnes@hotmail.com>
2024-01-08 01:35:24 +0000

    formatted none-ls.lua and added toml to prettier

--- lua/freesec/plugins/lsp/none-ls.lua
+++ lua/freesec/plugins/lsp/none-ls.lua
@@ -1,61 +1,60 @@
 return {
-  "nvimtools/none-ls.nvim", -- configure formatters & linters
-  lazy = true,
-  event = { "BufReadPre", "BufNewFile" }, -- to enable uncomment this
-dependencies = { "jay-babu/mason-null-ls.nvim", },
+	"nvimtools/none-ls.nvim", -- configure formatters & linters
+	lazy = true,
+	event = { "BufReadPre", "BufNewFile" }, -- to enable uncomment this
+	dependencies = { "jay-babu/mason-null-ls.nvim" },
 
-config = function()
-    local mason_null_ls = require("mason-null-ls")
+	config = function()
+		local mason_null_ls = require("mason-null-ls")
 
-    local null_ls = require("null-ls")
+		local null_ls = require("null-ls")
 
+		mason_null_ls.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"black", -- python formatter
+				"pylint", -- python linter
+				"eslint_d", -- js linter
+			},
+		})
+		-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.diagnostics.eslint,
+				null_ls.builtins.formatting.autopep8,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.prettier.with({ filetypes = { "json", "toml", "yaml", "markdown" } }),
+				null_ls.builtins.formatting.rustfmt,
+				null_ls.builtins.formatting.jq,
+				null_ls.builtins.formatting.shfmt,
+				null_ls.builtins.code_actions.gitsigns,
+			},
+			-- on_attach = function(client, bufnr)
+			-- 	if client.supports_method("textDocument/formatting") then
+			-- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			-- 		vim.api.nvim_create_autocmd("BufWritePre", {
+			-- 			group = augroup,
+			-- 			buffer = bufnr,
+			-- 			callback = function()
+			-- 				vim.lsp.buf.format({ bufnr = bufnr })
+			-- 			end,
+			-- 		})
+			-- 	end
+			-- end,
+		})
 
- mason_null_ls.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "black", -- python formatter
-        "pylint", -- python linter
-        "eslint_d", -- js linter
-      },
-    })
--- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.formatting.autopep8,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.prettier.with({ filetypes = { "json", "yaml", "markdown" } }),
-        null_ls.builtins.formatting.rustfmt,
-        null_ls.builtins.formatting.jq,
-        null_ls.builtins.formatting.shfmt,
-        null_ls.builtins.code_actions.gitsigns,
-    },
-    -- on_attach = function(client, bufnr)
-    -- 	if client.supports_method("textDocument/formatting") then
-    -- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    -- 		vim.api.nvim_create_autocmd("BufWritePre", {
-    -- 			group = augroup,
-    -- 			buffer = bufnr,
-    -- 			callback = function()
-    -- 				vim.lsp.buf.format({ bufnr = bufnr })
-    -- 			end,
-    -- 		})
-    -- 	end
-    -- end,
-})
+		local opts = { silent = false }
+		vim.api.nvim_set_keymap("n", "<A-f>", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+		vim.api.nvim_set_keymap("n", "<leader>F", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
 
-local opts = { silent = false }
-vim.api.nvim_set_keymap("n", "<A-f>", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>F", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
-
--- local lsp_formatting = function(bufnr)
--- 	vim.lsp.buf.format({
--- 		filter = function(client)
--- 			return client.name == "null-ls"
--- 		end,
--- 		bufnr = bufnr,
--- 	})
-end
+		-- local lsp_formatting = function(bufnr)
+		-- 	vim.lsp.buf.format({
+		-- 		filter = function(client)
+		-- 			return client.name == "null-ls"
+		-- 		end,
+		-- 		bufnr = bufnr,
+		-- 	})
+	end,
 }
