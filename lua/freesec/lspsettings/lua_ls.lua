c550af7b945ab5b874d82ba6ae85b6d5c3cca0f5
Frees3c <joeyjordanbarnes@hotmail.com>
2024-01-08 18:51:14 +0000

    messing around with lsp.. still.
    need to re-organise

---
+++ lua/freesec/lspsettings/lua_ls.lua
@@ -0,0 +1,24 @@
+return {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+            hint = {
+                enable = true,
+                arrayIndex = "Disable",
+                await = true,
+                paraName = "Disable",
+                paramType = false,
+                semicolon = "All",
+                setType = false,
+            },
+		},
+	},
+}
