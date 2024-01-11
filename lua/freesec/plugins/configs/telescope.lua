a52495bd967fb672b983a41d16ce0ce67cfbb71b
Frees3c <joeyjordanbarnes@hotmail.com>
2024-01-11 20:32:54 +0000

    added undo picker to telescope.

--- lua/freesec/plugins/configs/telescope.lua
+++ lua/freesec/plugins/configs/telescope.lua
@@ -4,8 +4,14 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"tsakirist/telescope-lazy.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-telescope/telescope-ui-select.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make"
+        },
+		{
+			"debugloop/telescope-undo.nvim",
+			lazy = true,
+		},
 	},
 
 	config = function()
@@ -31,19 +37,19 @@ return {
 					},
 					winblend = 20,
 				},
-                buffers = {
-                    theme = "dropdown",
-                    previewer = false,
-                    initial_mode = "normal",
-                    mappings = {
-                        i = {
-                            ["<C-d>"] = actions.delete_buffer,
-                        },
-                        n = {
-                            ["dd"] = actions.delete_buffer,
-                        },
-                    },
-                },
+				buffers = {
+					theme = "dropdown",
+					previewer = false,
+					initial_mode = "normal",
+					mappings = {
+						i = {
+							["<C-d>"] = actions.delete_buffer,
+						},
+						n = {
+							["dd"] = actions.delete_buffer,
+						},
+					},
+				},
 			},
 			extensions = {
 				fzf = {
@@ -64,6 +70,13 @@ return {
 						-- },
 					}),
 				},
+                undo = {
+                    layout_config = {
+                        height = 0.75,
+                        width = 0.75,
+                        prompt_position = "bottom",
+                    }
+                },
 			},
 			defaults = {
 				vimgrep_arguments = {
@@ -124,33 +137,34 @@ return {
 		require("telescope").load_extension("fzf")
 		require("telescope").load_extension("harpoon")
 		require("telescope").load_extension("lazy")
+        require("telescope").load_extension("undo")
 
 		-- custom previews
 		require("freesec.telescope-custom")
 
-        -- Keymaps 
+		-- Keymaps
 		local map = require("freesec.utils").map
 
 		map("n", "<leader>bb", ":Telescope buffers theme=dropdown<CR>")
 		map("n", "<leader>fb", ":Telescope current_buffer_fuzzy_find<CR>")
-        map("n", "<leader>fc", ":Telescope commands<cr>")
+		map("n", "<leader>fc", ":Telescope commands<cr>")
 		map("n", "<leader>fd", '<cmd>lua require"freesec.telescope-custom".dotfiles()<cr>')
 		map("n", "<leader>ff", ":Telescope find_files hidden=true<CR>")
 		map("n", "<leader>fg", ":Telescope git_files<CR>")
 		map("n", "<leader>fh", ":Telescope help_tags<CR>")
-        map("n", "<leader>fk", ":Telescope keymaps<CR>")
+		map("n", "<leader>fk", ":Telescope keymaps<CR>")
 		map("n", "<leader>fl", ":Telescope lazy<CR>")
 		map("n", "<leader>fm", ":Telescope harpoon marks theme=dropdown<CR>")
-        map("n", "<leader>fM", ":Telescope man_pages<CR>")
+		map("n", "<leader>fM", ":Telescope man_pages<CR>")
 		map("n", "<leader>fp", ":Telescope find_files<CR>")
 		map("n", "<leader>fq", ":Telescope quickfix<CR>")
 		map("n", "<leader>fr", ":Telescope oldfiles <CR>")
-        map("n", "<leader>fR", ":Telescope registers<CR>")
+		map("n", "<leader>fR", ":Telescope registers<CR>")
 		map("n", "<leader>fs", ":Telescope grep_string<cr>")
+        map("n", "<leader>fu", ":Telescope undo<CR>")
 		map("n", "<leader>fw", ":Telescope live_grep<CR>")
 		map("n", "<leader>fx", ":Telescope git_status<cr>")
-        map("n", "<leader>gc", ":Telescope git_commits<cr>")
+		map("n", "<leader>gc", ":Telescope git_commits<cr>")
 		map("n", "<leader>tr", ":Telescope resume<CR>")
-
 	end,
 }
