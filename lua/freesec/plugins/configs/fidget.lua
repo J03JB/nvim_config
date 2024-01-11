ec4d47f1e7be42a949efa30198496dfbfb3fd8a0
Frees3c <joeyjordanbarnes@hotmail.com>
2024-01-11 20:31:30 +0000

    messing around with fidget.nvim

--- lua/freesec/plugins/configs/fidget.lua
+++ lua/freesec/plugins/configs/fidget.lua
@@ -1,10 +1,23 @@
 return {
 
-    "j-hui/fidget.nvim",
+	"j-hui/fidget.nvim",
 	event = "LspAttach",
 	lazy = true,
 
 	config = function()
-		require("fidget").setup({})
+		require("fidget").setup({
+			progress = {
+				poll_rate = 100,
+				ignore_done_already = true,
+				display = {
+					done_ttl = 0.5,
+					-- Icon shown when LSP progress tasks are in progress
+					progress_icon = { pattern = "meter", period = 2 },
+				},
+			},
+			notification = {
+				-- override_vim_notify = true,
+			},
+		})
 	end,
 }
