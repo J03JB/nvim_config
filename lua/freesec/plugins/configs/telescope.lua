fdbd58100f433d599d027fee5a1b98f8351f98be
Frees3c <joeyjordanbarnes@hotmail.com>
2024-02-06 21:12:18 +0000

    moved telescope keymaps to Telescope.lua

--- lua/freesec/plugins/configs/telescope.lua
+++ lua/freesec/plugins/configs/telescope.lua
@@ -4,15 +4,39 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"tsakirist/telescope-lazy.nvim",
-		"nvim-telescope/telescope-ui-select.nvim",
+		--     "nvim-telescope/telescope-ui-select.nvim",
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "make",
 		},
-		{
-			"debugloop/telescope-undo.nvim",
-			lazy = true,
-		},
+		"debugloop/telescope-undo.nvim",
+	},
+
+	lazy = true,
+
+	keys = {
+		{ "<leader>bb", "<cmd>Telescope buffers theme=dropdown<CR>",       desc = "show buffers" },
+		{ "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find<CR>",    desc = "search buffer" },
+		{ "<leader>fc", "<cmd>Telescope commands<cr>",                     desc = "Telescope commands" },
+		{ "<leader>fd", '<cmd>lua require"freesec.telescope-custom".dotfiles()<cr>', desc = "show dotfiles" },
+		{ "<leader>ff", "<cmd>Telescope find_files hidden=true<CR>",       desc = "find files" },
+		{ "<leader>fg", "<cmd>Telescope git_files<CR>",                    desc = "search git files" },
+		{ "<leader>fh", "<cmd>Telescope help_tags<CR>",                    desc = "search help files" },
+		{ "<leader>fk", "<cmd>Telescope keymaps<CR>",                      desc = "show keymaps" },
+		{ "<leader>fl", "<cmd>Telescope lazy<CR>",                         desc = "show installed plugins" },
+		{ "<leader>fm", "<cmd>Telescope harpoon marks theme=dropdown<CR>", desc = "show harpoon marks" },
+		{ "<leader>fM", "<cmd>Telescope man_pages<CR>",                    desc = "search man pages" },
+		{ "<leader>fp", "<cmd>Telescope find_files<CR>",                   desc = "find files" },
+		{ "<leader>fq", "<cmd>Telescope quickfix<CR>",                     desc = "search quickfix" },
+		{ "<leader>fr", "<cmd>Telescope oldfiles <CR>",                    desc = "show recent files" },
+		{ "<leader>fR", "<cmd>Telescope registers<CR>",                    desc = "show registers" },
+		{ "<leader>fs", "<cmd>Telescope grep_string<cr>",                  desc = "search word under cursor" },
+		{ "<leader>fu", "<cmd>Telescope undo<CR>",                         desc = "show undo list" },
+		{ "<leader>fw", "<cmd>Telescope live_grep<cr>",                    desc = "Live grep" },
+		{ "<leader>fx", "<cmd>Telescope git_status<cr>",                   desc = "git status" },
+		{ "<leader>gc", "<cmd>Telescope git_commits<cr>",                  desc = "show git commits" },
+		{ "<leader>ss", "<cmd>Telescope spell_suggest theme=dropdown<cr>", desc = "spellig suggestions" },
+		{ "<leader>tr", "<cmd>Telescope resume<CR>",                       desc = "" },
 	},
 
 	config = function()
@@ -64,13 +88,13 @@ return {
 						height = 0.5,
 					},
 				},
-				["ui-select"] = {
-					require("telescope.themes").get_dropdown({
-						-- layout_config = {
-						-- prompt_position = "top",
-						-- },
-					}),
-				},
+				-- ["ui-select"] = {
+				-- 	require("telescope.themes").get_dropdown({
+				-- 		-- layout_config = {
+				-- 		-- prompt_position = "top",
+				-- 		-- },
+				-- 	}),
+				-- },
 				undo = {
 					layout_config = {
 						height = 0.75,
@@ -102,14 +126,33 @@ return {
 					horizontal = { mirror = false },
 					vertical = { mirror = false },
 				},
+				preview = { treesitter = false },
 				file_sorter = require("telescope.sorters").get_fzy_sorter,
-                file_ignore_patterns = {
-                    '%.git/', 'node_modules/', '%.npm/', '__pycache__/', '%[Cc]ache/',
-                    '%.dropbox/', '%.dropbox_trashed/', '%.local/share/Trash/',
-                    '%.py[c]', '%.sw.?', '~$', '%.tags', '%.gemtags', '%.tmp',
-                    '%.plist$', '%.pdf$', '%.jpg$', '%.JPG$', '%.jpeg$', '%.png$',
-                    '%.class$', '%.pdb$', '%.dll$'
-                },
+				file_ignore_patterns = {
+					"%.git/",
+					"node_modules/",
+					"%.npm/",
+					"__pycache__/",
+					"%[Cc]ache/",
+					"%.dropbox/",
+					"%.dropbox_trashed/",
+					"%.local/share/Trash/",
+					"%.py[c]",
+					"%.sw.?",
+					"~$",
+					"%.tags",
+					"%.gemtags",
+					"%.tmp",
+					"%.plist$",
+					"%.pdf$",
+					"%.jpg$",
+					"%.JPG$",
+					"%.jpeg$",
+					"%.png$",
+					"%.class$",
+					"%.pdb$",
+					"%.dll$",
+				},
 				generic_sorter = require("telescope.sorters").get_fzy_sorter,
 				path_display = { "full" },
 				winblend = 0,
@@ -140,39 +183,10 @@ return {
 				},
 			},
 		})
-		require("telescope").load_extension("ui-select")
+		-- require("telescope").load_extension("ui-select")
 		require("telescope").load_extension("fzf")
-		require("telescope").load_extension("harpoon")
-		require("telescope").load_extension("lazy")
-		require("telescope").load_extension("undo")
 
 		-- custom previews
 		require("freesec.telescope-custom")
-
-		-- Keymaps
-		local map = require("freesec.utils").map
-
-		map("n", "<leader>bb", ":Telescope buffers theme=dropdown<CR>")
-		map("n", "<leader>fb", ":Telescope current_buffer_fuzzy_find<CR>")
-		map("n", "<leader>fc", ":Telescope commands<cr>")
-		map("n", "<leader>fd", '<cmd>lua require"freesec.telescope-custom".dotfiles()<cr>')
-		map("n", "<leader>ff", ":Telescope find_files hidden=true<CR>")
-		map("n", "<leader>fg", ":Telescope git_files<CR>")
-		map("n", "<leader>fh", ":Telescope help_tags<CR>")
-		map("n", "<leader>fk", ":Telescope keymaps<CR>")
-		map("n", "<leader>fl", ":Telescope lazy<CR>")
-		map("n", "<leader>fm", ":Telescope harpoon marks theme=dropdown<CR>")
-		map("n", "<leader>fM", ":Telescope man_pages<CR>")
-		map("n", "<leader>fp", ":Telescope find_files<CR>")
-		map("n", "<leader>fq", ":Telescope quickfix<CR>")
-		map("n", "<leader>fr", ":Telescope oldfiles <CR>")
-		map("n", "<leader>fR", ":Telescope registers<CR>")
-		map("n", "<leader>fs", ":Telescope grep_string<cr>")
-		map("n", "<leader>fu", ":Telescope undo<CR>")
-		map("n", "<leader>fw", ":Telescope live_grep<CR>")
-		map("n", "<leader>fx", ":Telescope git_status<cr>")
-		map("n", "<leader>gc", ":Telescope git_commits<cr>")
-        map("n", "<leader>ss", ":Telescope spell_suggest theme=dropdown<cr>")
-		map("n", "<leader>tr", ":Telescope resume<CR>")
 	end,
 }
