6a1aac3caa1a77a7dc62dd715cfc2877f2dfa61d
Frees3c <joeyjordanbarnes@hotmail.com>
2024-02-21 22:50:38 +0000

    feat: switched from harpoon to harpoon2

--- lua/freesec/plugins/configs/harpoon.lua
+++ lua/freesec/plugins/configs/harpoon.lua
@@ -1,33 +1,34 @@
 return {
-	"ThePrimeagen/harpoon",
+	--     enabled = false,
+	-- "ThePrimeagen/harpoon",
 
-    keys = {
-        { "<C-l>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", desc = "Show harpoon menu"      },
-        { "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<CR>",    desc = "Add file to harpoon"    },
-        { "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<CR>",     desc = "File 1 harpoon"         },
-        { "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<CR>",     desc = "File 2 harpoon"         },
-        { "<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<CR>",     desc = "File 3 harpoon"         },
-        { "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<CR>",     desc = "File 4 harpoon"         },
-        { "<A-[>",     "<cmd>lua require('harpoon.ui').nav_prev()<CR>",      desc = "Previous mark  harpoon" },
-        { "<A-]>",     "<cmd>lua require('harpoon.ui').nav_next()<CR>",      desc = "Next  mark  harpoon"    },
-    },
+	--     keys = {
+	--         { "<C-l>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", desc = "Show harpoon menu"      },
+	--         { "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<CR>",    desc = "Add file to harpoon"    },
+	--         { "<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<CR>",     desc = "File 1 harpoon"         },
+	--         { "<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<CR>",     desc = "File 2 harpoon"         },
+	--         { "<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<CR>",     desc = "File 3 harpoon"         },
+	--         { "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<CR>",     desc = "File 4 harpoon"         },
+	--         { "<A-[>",     "<cmd>lua require('harpoon.ui').nav_prev()<CR>",      desc = "Previous mark  harpoon" },
+	--         { "<A-]>",     "<cmd>lua require('harpoon.ui').nav_next()<CR>",      desc = "Next  mark  harpoon"    },
+	--     },
 
-	config = function()
-		local ui = require("harpoon.ui")
-		local tmux = require("harpoon.tmux")
-		vim.keymap.set("n", "<A-t>", function()
-			tmux.gotoTerminal(1)
-		end)
+	-- config = function()
+	-- 	local ui = require("harpoon.ui")
+	-- 	local tmux = require("harpoon.tmux")
+	-- 	vim.keymap.set("n", "<A-t>", function()
+	-- 		tmux.gotoTerminal(1)
+	-- 	end)
 
-		-- Fix for macos and Alt key.
-		vim.keymap.set("n", "‘", function()
-			ui.nav_next()
-		end)
-		vim.keymap.set("n", "“", function()
-			ui.nav_prev()
-		end)
-		vim.keymap.set("n", "†", function()
-			tmux.gotoTerminal(1)
-		end)
-	end,
+	-- 	-- Fix for macos and Alt key.
+	-- 	vim.keymap.set("n", "‘", function()
+	-- 		ui.nav_next()
+	-- 	end)
+	-- 	vim.keymap.set("n", "“", function()
+	-- 		ui.nav_prev()
+	-- 	end)
+	-- 	vim.keymap.set("n", "†", function()
+	-- 		tmux.gotoTerminal(1)
+	-- 	end)
+	-- end,
 }
