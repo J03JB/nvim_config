ddfacf15ebf7fc27f1bd472542d6078681560c6a
Frees3c <>
2024-02-23 21:47:43 +0000

    git sign keymaps + signs

--- lua/freesec/plugins/configs/gitsigns.lua
+++ lua/freesec/plugins/configs/gitsigns.lua
@@ -5,26 +5,11 @@ return {
 		local map = require("freesec.utils").map
 		require("gitsigns").setup({
 			signs = {
-				add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-				change = {
-					hl = "GitSignsChange",
-					text = "│",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
-				},
-				delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-				topdelete = {
-					hl = "GitSignsDelete",
-					text = "‾",
-					numhl = "GitSignsDeleteNr",
-					linehl = "GitSignsDeleteLn",
-				},
-				changedelete = {
-					hl = "GitSignsChange",
-					text = "~",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
-				},
+				add = { text = "+" },
+				change = { text = "|" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
 			},
 			numhl = true,
 			linehl = false,
@@ -37,7 +22,7 @@ return {
 			attach_to_untracked = true,
 			status_formatter = nil, -- Use default
 			preview_config = {
-				border = "single",
+				border = "rounded",
 				style = "minimal",
 				relative = "cursor",
 				row = 0,
@@ -46,18 +31,18 @@ return {
 		})
 
 		-- Keymaps.
-		map("n", "]c", '<cmd>lua require"gitsigns".next_hunk()<CR>')
-		map("n", "[c", '<cmd>lua require"gitsigns".prev_hunk()<CR>')
-		map("n", "<leader>hs", '<cmd>lua require"gitsigns".stage_hunk()<CR>')
-		map("n", "<leader>hu", '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>')
-		map("n", "<leader>hr", '<cmd>lua require"gitsigns".reset_hunk()<CR>')
-		map("n", "<leader>hR", '<cmd>lua require"gitsigns".reset_buffer()<CR>')
-		map("n", "<leader>hp", '<cmd>lua require"gitsigns".preview_hunk()<CR>')
+		map("n", "]g",         '<cmd>lua require"gitsigns".next_hunk()<CR>')
+		map("n", "[g",         '<cmd>lua require"gitsigns".prev_hunk()<CR>')
+		map("x", "ih",         '<cmd>lua require"gitsigns".select_hunk()<CR>')
 		map("n", "<leader>hb", '<cmd>lua require"gitsigns".blame_line()<CR>')
 		map("n", "<leader>hd", "<cmd>Gitsigns diffthis<CR>")
 		map("n", "<leader>hD", '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-		map("n", "<leader>td", "<cmd>Gitsigns toggle_deleted<CR>")
+		map("n", "<leader>hp", '<cmd>lua require"gitsigns".preview_hunk()<CR>')
+		map("n", "<leader>hr", '<cmd>lua require"gitsigns".reset_hunk()<CR>')
+		map("n", "<leader>hR", '<cmd>lua require"gitsigns".reset_buffer()<CR>')
+		map("n", "<leader>hs", '<cmd>lua require"gitsigns".stage_hunk()<CR>')
+		map("n", "<leader>hu", '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>')
 		map("n", "<leader>sh", '<cmd>lua require"gitsigns".select_hunk()<CR>')
-		map("x", "ih", '<cmd>lua require"gitsigns".select_hunk()<CR>')
+		map("n", "<leader>td", "<cmd>Gitsigns toggle_deleted<CR>")
 	end,
 }
