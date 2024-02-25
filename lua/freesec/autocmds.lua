908300617b6dce8c72efecd15e6596bbb57f8b61
Frees3c <>
2024-02-25 00:52:12 +0000

    added q to close quickfix list

--- lua/freesec/autocmds.lua
+++ lua/freesec/autocmds.lua
@@ -10,16 +10,22 @@ require("freesec.utils").create_augroups({
 		{ "InsertLeave,WinEnter", "*", "set cursorline" },
 		{ "InsertEnter,WinLeave", "*", "set nocursorline" },
 		-- Restore cursor position when opening file
-        { "BufReadPost", "*", 'silent! normal! g`"zv' },
-		-- Format Json files using `jq`
-		-- { "BufWritePost", "*.json", ":%!jq ." },
+		{ "BufReadPost", "*", 'silent! normal! g`"zv' },
 		-- don't continue comment on next line
 		{ "FileType", "*", "setlocal formatoptions-=cro" },
 		-- open help in vertical split instead of horizontal
 		{ "BufWinEnter", "*", [[if &buftype == 'help' | wincmd L | endif]] },
 		-- resizes splits when terminal gets resized
-		{ "WinResized",	"*","wincmd ="	},
+		{ "WinResized", "*", "wincmd =" },
 		-- set spell for markdown, txt and git commit
-		{ "FileType", "gitcommit,markdown,txt","setlocal spell" },
+		{ "FileType", "gitcommit,markdown,txt", "setlocal spell" },
+        -- close quickfix with 'q'
+        { "FileType", "qf", [[ nnoremap <buffer> q :close<CR> | set nobuflisted ]], },
+        -- cloe neovim if quickfix is the last window
+		{
+			"WinEnter",
+			"*",
+			[[  if winnr('$') == 1 && &buftype == "quickfix"|q|endif ]],
+		},
 	},
 })
