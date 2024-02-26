adf49fce9c7130f6ac0e693656ecd80dcb355f5c
Frees3c <joeyjordanbarnes@hotmail.com>
2024-02-26 22:59:46 +0000

    fixed typo in lualine.lua

--- lua/freesec/plugins/configs/lualine.lua
+++ lua/freesec/plugins/configs/lualine.lua
@@ -50,7 +50,7 @@ return {
 				-- section_separators = { left = "", right = "" },
 				-- component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
-				ciiiiiiomponent_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
 			},
 			sections = {
 				lualine_a = { "mode" },
