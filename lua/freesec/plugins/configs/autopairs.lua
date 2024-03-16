51a55400b081f6a11a96b2757c862a6d14a922bc
Frees3c <>
2024-03-16 20:19:34 +0000

    moved cmp function into cmp.lua

--- lua/freesec/plugins/configs/autopairs.lua
+++ lua/freesec/plugins/configs/autopairs.lua
@@ -3,12 +3,5 @@ return {
     event = "InsertEnter",
     config = function()
         require("nvim-autopairs").setup({})
-
-        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-        local cmp = require('cmp')
-        cmp.event:on(
-            'confirm_done',
-            cmp_autopairs.on_confirm_done()
-        )
     end
 }
