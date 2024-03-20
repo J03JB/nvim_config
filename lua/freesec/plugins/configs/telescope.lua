3b5a65f566e9e1dc1398081160ce41ff85f5882f
JB <joeyjordanbarnes@hotmail.com>
2024-03-20 21:40:58 +0000

    Removed unused telescope mappings/settings

--- lua/freesec/plugins/configs/telescope.lua
+++ lua/freesec/plugins/configs/telescope.lua
@@ -15,28 +15,27 @@ return {
 	lazy = true,
 
 	keys = {
+        -- telescope-custom.lua
+        { "<leader>ev", "<cmd>lua require'freesec.telescope-custom'.edit_neovim()<cr>", desc = "nvim config" },
+		{ "<leader>fd", '<cmd>lua require"freesec.telescope-custom".dotfiles()<cr>', desc = "show dotfiles" },
+        -- Telescope builtins
 		{ "<leader>bb", "<cmd>Telescope buffers theme=dropdown<CR>",       desc = "show buffers" },
 		{ "<leader>fb", "<cmd>Telescope current_buffer_fuzzy_find<CR>",    desc = "search buffer" },
-		{ "<leader>fc", "<cmd>Telescope commands<cr>",                     desc = "Telescope commands" },
-		{ "<leader>fd", '<cmd>lua require"freesec.telescope-custom".dotfiles()<cr>', desc = "show dotfiles" },
+		{ "<leader>fc", "<cmd>Telescope commands<cr>",                     desc = "telescope commands" },
 		{ "<leader>ff", "<cmd>Telescope find_files hidden=true<CR>",       desc = "find files" },
 		{ "<leader>fg", "<cmd>Telescope git_files<CR>",                    desc = "search git files" },
 		{ "<leader>fh", "<cmd>Telescope help_tags<CR>",                    desc = "search help files" },
 		{ "<leader>fk", "<cmd>Telescope keymaps<CR>",                      desc = "show keymaps" },
 		{ "<leader>fl", "<cmd>Telescope lazy<CR>",                         desc = "show installed plugins" },
-		{ "<leader>fm", "<cmd>Telescope harpoon marks theme=dropdown<CR>", desc = "show harpoon marks" },
 		{ "<leader>fM", "<cmd>Telescope man_pages<CR>",                    desc = "search man pages" },
 		{ "<leader>fp", "<cmd>Telescope find_files<CR>",                   desc = "find files" },
 		{ "<leader>fq", "<cmd>Telescope quickfix<CR>",                     desc = "search quickfix" },
 		{ "<leader>fr", "<cmd>Telescope oldfiles <CR>",                    desc = "show recent files" },
-		{ "<leader>fR", "<cmd>Telescope registers<CR>",                    desc = "show registers" },
 		{ "<leader>fs", "<cmd>Telescope grep_string<cr>",                  desc = "search word under cursor" },
 		{ "<leader>fu", "<cmd>Telescope undo<CR>",                         desc = "show undo list" },
-		{ "<leader>fw", "<cmd>Telescope live_grep<cr>",                    desc = "Live grep" },
-		{ "<leader>fx", "<cmd>Telescope git_status<cr>",                   desc = "git status" },
+		{ "<leader>fw", "<cmd>Telescope live_grep<cr>",                    desc = "live grep" },
 		{ "<leader>gc", "<cmd>Telescope git_commits<cr>",                  desc = "show git commits" },
 		{ "<leader>ss", "<cmd>Telescope spell_suggest theme=dropdown<cr>", desc = "spellig suggestions" },
-		{ "<leader>tr", "<cmd>Telescope resume<CR>",                       desc = "" },
 	},
 
 	config = function()
@@ -44,7 +43,6 @@ return {
 
 		-- Global remapping
 		------------------------------
-		-- '--color=never',
 		require("telescope").setup({
 			pickers = {
 				find_files = {
@@ -109,12 +107,6 @@ return {
 					"--smart-case",
 					"--column",
 				},
-				prompt_prefix = "  ",
-				selection_caret = " ",
-				entry_prefix = "  ",
-				initial_mode = "insert",
-				selection_strategy = "reset",
-				sorting_strategy = "descending",
 				layout_strategy = "horizontal",
 				layout_config = {
 					width = 0.75,
@@ -151,39 +143,33 @@ return {
 					"%.dll$",
 				},
 				generic_sorter = require("telescope.sorters").get_fzy_sorter,
-				path_display = { "full" },
 				winblend = 10,
 				border = {},
 				borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
 				color_devicons = true,
 				use_less = true,
 				set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-				file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-				grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-				qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-				-- Developer configurations: Not meant for general override
-				buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
-				mappings = {
-					i = {
-						["<C-n>"] = actions.cycle_history_next,
-						["<C-p>"] = actions.cycle_history_prev,
-						["<C-c>"] = actions.close,
-						["<C-j>"] = actions.move_selection_next,
-						["<C-k>"] = actions.move_selection_previous,
-						["<esc>"] = actions.close,
-						["<CR>"] = actions.select_default + actions.center,
-					},
-					n = {
-						["<C-j>"] = actions.move_selection_next,
-						["<C-k>"] = actions.move_selection_previous,
-					},
-				},
+                mappings = {
+                    i = {
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-k>"] = actions.move_selection_previous,
+                        ["<esc>"] = actions.close
+                    },
+                    n = {
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-k>"] = actions.move_selection_previous,
+                    },
+                }
 			},
 		})
+
+        -- Extensions
 		require("telescope").load_extension("fzf")
 		require("telescope").load_extension("ui-select")
 
-		-- custom previews
-		require("freesec.telescope-custom")
+        vim.keymap.set('n', '<leader>fW', function()
+            local word = vim.fn.expand("<cWORD>")
+            require("telescope.builtin").grep_string({ search = word })
+        end)
 	end,
 }
