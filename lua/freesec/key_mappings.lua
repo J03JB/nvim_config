ac08a6d922a81e911de885ffae97d858dedfcca1
JB <joeyjordanbarnes@hotmail.com>
2024-03-21 08:53:07 +0000

    added keymaps for quickfix list

--- lua/freesec/key_mappings.lua
+++ lua/freesec/key_mappings.lua
@@ -27,28 +27,31 @@ map("n", "<leader>w",  "g~iw")                -- toggle capitalisation
 map("n", "<leader>O",  "moO<ESC>")            -- insert line above
 map("n", "<leader>o",  "moo<ESC>")            -- insert line below
 map("n", "<leader>h",  ":nohlsearch<CR>")     -- Clear highlights
-map("n", "<C-k>",      ":m-2<cr>")            -- move line down
-map("n", "<C-j>",      ":m+<cr>")             -- move line up
+map("n", "J",          ":m+<cr>")             -- move line up
 map("n", "<leader>x",  "<cmd>!chmod +x %<CR>")-- make file executable
 map("n", "U",          "<nop>")               -- unmap 'U'
-map("n", "<leader>y",  '"+y')                 -- copy to system clipboard
-map("x", "<leader>y",  '"+y')                 -- copy to system clipboard in visual mode
-map("n", "<leader>Y",  '"+Y')                 -- copy line to system clipboard
+map("n", "<leader>y",  [["+y]])               -- copy to system clipboard
+map("n", "<leader>Y",  [["+Y]])               -- copy line to system clipboard
 map("n", "<C-h>",      "<C-w>h")              -- ctrl + h to go to left pane
 map("n", "H",          "^")                   -- H to go to start of line
 map("n", "L",          "$")                   -- L to go to end of line
 map("n", "Q",          "@qj")                 -- Q to repeat macro and move down a line
-map("n", "<leader>xp", ":call setreg('+', getreg('@'))<CR>") -- copy paste register to system clipboard
-map("n", "<F9>",       ":lcd %:p:h<CR>", { silent = false }) -- change to cwd
 map("n", "[b",         ":bprev<CR>")          -- go to previous buffer
 map("n", "]b",         ":bnext<CR>")          -- go to next buffer
+map("n", "<C-k>",      "<cmd>cprev<CR>zz")    -- go to previous item in quickfix
+map("n", "<leader>k",  "<cmd>lnext<CR>zz")    -- go to next item in loclist
+map("n", "<leader>j",  "<cmd>lprev<CR>zz")    -- go to previous item in loclist
+map("n", "<C-j>",      "<cmd>cnext<CR>zz")    -- go to next item in quickfix
+map("n", "<C-d>",      "<C-d>zz")             -- page down and center
+map("n", "<C-u>",      "<C-u>zz")             -- page up and center
+map("n", "<leader>xp", ":call setreg('+', getreg('@'))<CR>") -- copy paste register to system clipboard
+map("n", "<F9>",       ":lcd %:p:h<CR>", { silent = false }) -- change to cwd
 
 map("i", "<c-j>",      "<esc>ji")             -- ctrl + j to down a line in insert mode
 map("i", "<c-k>",      "<esc>ki")             -- ctrl + k to move up a line in insert mode
 map("i", "<c-h>",      "<esc>i")              -- ctrl + h to move left in insert mode
 map("i", "<c-l>",      "<esc>la")             -- ctrl + l to move right in insert mode
 map("i", "jk",         "<Esc>")               -- "jk" as escape key in insert mode
-map("i", "<C-c>",      "<Esc>")               -- ctrl + c as escape key in insert mode
 map("i", "<C-BS>",     "<Esc>cvb")            -- Ctrl + backspace to delete word (insert mode)
 map("i", "<A-;>",      "<Esc>miA;<Esc>`ii")   -- insert semicolon at the end of line
 map("i", "…",          "<Esc>miA;<Esc>`ii")   -- mac keymap for above.
@@ -59,8 +62,9 @@ map("v", ">",          ">gv")                 -- better indentation
 map("v", "<",          "<gv")                 -- better indentation
 map("v", "J",          ":move '>+1<CR>gv=gv") -- Move selected line / block of text down in visual mode
 map("v", "K",          ":move '<-2<CR>gv=gv") -- Move selected line / block of text up in visual mode
-map("x", "p",          '"_dP')                -- replace selected text in visual mode. sends to blackhole.
+map("x", "<leader>p",          '"_dP')                -- replace selected text in visual mode. sends to blackhole.
 map("x", "Q",          ":norm @q<CR>")        -- Q in visual mode to repeat macro
+map("x", "<leader>y",  [["+y]])               -- copy to system clipboard in visual mode
 
 
 -- Search and replace word under cursor
@@ -68,7 +72,7 @@ map("n", "<leader>s",  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {
 -- Same but for 'big word'
 map("n", "<leader>S",  [[:%s/\<<C-r><C-a>\>/<C-r><C-a>/gI<Left><Left><Left>]], { silent = false })
 
--- dd doesn't yank empty line to default register
+-- Don't yank to register if line is empty
 vim.keymap.set("n", "dd", function()
 	if vim.api.nvim_get_current_line():match("^%s*$") then
 		return '"_dd'
