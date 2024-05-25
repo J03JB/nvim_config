e73869d988d5d616f3a8656549be6013d563044d
JB <joeyjordanbarnes@hotmail.com>
2024-05-25 21:12:42 +0100

    feat: conform.nvim setup range formatting

--- lua/jb/plugins/lsp/conform.lua
+++ lua/jb/plugins/lsp/conform.lua
@@ -2,17 +2,6 @@ return {
 	"stevearc/conform.nvim",
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
-	keys = {
-		{
-			-- Customize or remove this keymap to your liking
-			"<leader>F",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			mode = "",
-			desc = "Format buffer",
-		},
-	},
 	-- Everything in opts will be passed to setup()
 	opts = {
 		-- Define your formatters
@@ -38,4 +27,21 @@ return {
 		-- If you want the formatexpr, here is the place to set it
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 	end,
+
+    -- Keymaps
+    -- formatting for visual selection 
+    vim.api.nvim_create_user_command("Format", function(args)
+        local range = nil
+        if args.count ~= -1 then
+            local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+            range = {
+                start = { args.line1, 0 },
+                ["end"] = { args.line2, end_line:len() },
+            }
+        end
+        require("conform").format({ async = true, lsp_fallback = true, range = range })
+    end, { range = true }),
+
+    vim.keymap.set({"n", "v"}, "<leader>F", ":Format<CR>", { desc = "Format"})
+
 }
