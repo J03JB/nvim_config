397022e39ec1919aca8a6e09079d5ff3f96198a3
JB <joeyjordanbarnes@hotmail.com>
2025-01-08 20:22:39 +0000

    fixed lsp

---
+++ lsp/ts_ls.lua
@@ -0,0 +1,6 @@
+vim.lsp.config["ts_ls"] = {
+	init_options = { hostInfo = "neovim" },
+	cmd = { "typescript-language-server", "--stdio" },
+	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+	single_file_support = true,
+}
