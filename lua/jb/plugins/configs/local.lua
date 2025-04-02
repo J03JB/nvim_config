329fad4f334ab6fe861c7fae9e63590f84a97f12
JB <joeyjordanbarnes@hotmail.com>
2025-04-02 21:48:18 +0100

    i broke something

--- lua/jb/plugins/configs/local.lua
+++ lua/jb/plugins/configs/local.lua
@@ -15,6 +15,30 @@ local M = {
 	-- 	end,
 	-- },
 	--    { dir = "~/Dev/Plugins/fzf-undo/" },
+	-- { dir = "~/Dev/Plugins/teleprompt.nvim" },
+	-- {
+	-- 	dir = "~/Dev/Plugins/gitbar.nvim",
+	-- 	dev = true,
+	--    },
+	-- {
+	-- 	"J03JB/pasta.nvim",
+	-- 	dir = "~/Dev/Plugins/pasta.nvim",
+	-- 	init = function()
+	-- 		require("pasta").setup()
+	-- 	end,
+	-- },
+	--    { dir = "~/Dev/Plugins/fzf-undo/" },
+-- 		dir = "~/Dev/Plugins/gitbar.nvim",
+-- 		dev = true,
+--     },
+-- 	{
+-- 		"J03JB/pasta.nvim",
+-- 		dir = "~/Dev/Plugins/pasta.nvim",
+-- 		init = function()
+-- 			require("pasta").setup()
+-- 		end,
+-- 	},
+--     { dir = "~/Dev/Plugins/fzf-undo/" },
 }
 
 return M
