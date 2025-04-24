dcecbf6cff2fe8af59cc3875e7f4b4b9ced78cb6
JB <joeyjordanbarnes@hotmail.com>
2025-04-25 00:01:36 +0100

    added roslyn config, for c# dev.

---
+++ lua/jb/plugins/configs/roslyn.lua
@@ -0,0 +1,37 @@
+return {
+  "seblyng/roslyn.nvim",
+  ft = "cs",
+  opts = {
+    config = {
+      settings = {
+        ["csharp|inlay_hints"] = {
+          csharp_enable_inlay_hints_for_implicit_object_creation = true,
+          csharp_enable_inlay_hints_for_implicit_variable_types = true,
+          csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+          csharp_enable_inlay_hints_for_types = true,
+          dotnet_enable_inlay_hints_for_indexer_parameters = true,
+          dotnet_enable_inlay_hints_for_literal_parameters = true,
+          dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+          dotnet_enable_inlay_hints_for_other_parameters = true,
+          dotnet_enable_inlay_hints_for_parameters = true,
+          dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+          dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+          dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+        },
+        ["csharp|code_lens"] = {
+          dotnet_enable_references_code_lens = true,
+        },
+        ["csharp|completion"] = {
+          dotnet_show_completion_items_from_unimported_namespaces = true,
+          dotnet_show_name_completion_suggestions = true,
+        },
+        ["csharp|background_analysis"] = {
+          background_analysis_dotnet_compiler_diagnostics_scope = "fullSolution",
+        },
+        ["csharp|symbol_search"] = {
+          dotnet_search_reference_assemblies = true,
+        },
+      },
+    },
+  },
+}
