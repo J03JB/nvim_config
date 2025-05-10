5722337cf0448296d7c562b8efef4f18457b523f
JB <joeyjordanbarnes@hotmail.com>
2025-05-10 19:35:47 +0100

    trying out rustowl for lifetimes in rust

---
+++ lua/jb/plugins/configs/rustowl.lua
@@ -0,0 +1,15 @@
+return {
+  "cordx56/rustowl",
+  version = "*", -- Latest stable version
+  build = "cargo binstall rustowl",
+  lazy = false,
+  opts = {
+    client = {
+      on_attach = function(_, buffer)
+        vim.keymap.set("n", "<leader>ro", function()
+          require("rustowl").toggle(buffer)
+        end, { buffer = buffer, desc = "Toggle RustOwl" })
+      end,
+    },
+  },
+}
