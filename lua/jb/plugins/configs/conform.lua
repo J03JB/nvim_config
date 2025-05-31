87c990f08319403bd11a29338edddb22954833ae
JB <joeyjordanbarnes@hotmail.com>
2025-05-31 22:07:19 +0100

    chore(format): format lua files

--- lua/jb/plugins/configs/conform.lua
+++ lua/jb/plugins/configs/conform.lua
@@ -1,46 +1,50 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	-- Everything in opts will be passed to setup()
-	opts = {
-		-- Define your formatters
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "autopep8", "black" },
-			javascript = { { "prettierd", "prettier" } },
-			json = { "jq" },
-			yaml = { "yamlfmt" },
-			go = { "gofumpt" },
-			markdown = { "prettier", "markdownlint" },
-		},
-		-- Set up format-on-save
-		-- format_on_save = { timeout_ms = 500, lsp_fallback = true },
-		-- Customize formatters
-		formatters = {
-			shfmt = {
-				prepend_args = { "-i", "2" },
-			},
-		},
-	},
-	init = function()
-		-- If you want the formatexpr, here is the place to set it
-		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-	end,
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  opts = {
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+    formatters_by_ft = {
+      cs = { "csharpier" },
+      lua = { "stylua" },
+      python = { "autopep8", "black" },
+      javascript = { { "prettierd", "prettier" } },
+      json = { "jq" },
+      yaml = { "yamlfmt" },
+      go = { "gofumpt" },
+      markdown = { "prettier", "markdownlint" },
+    },
+    -- Set up format-on-save
+    -- format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "2" },
+      },
+      csharpier = {
+        command = "dotnet-csharpier",
+        args = { "--write-stdout" },
+      },
+    },
+  },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 
-	-- Keymaps
-	-- formatting for visual selection
-	vim.api.nvim_create_user_command("Format", function(args)
-		local range = nil
-		if args.count ~= -1 then
-			local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-			range = {
-				start = { args.line1, 0 },
-				["end"] = { args.line2, end_line:len() },
-			}
-		end
-		require("conform").format({ async = true, lsp_fallback = true, range = range })
-	end, { range = true }),
+  -- formatting for visual selection
+  vim.api.nvim_create_user_command("Format", function(args)
+    local range = nil
+    if args.count ~= -1 then
+      local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+      range = {
+        start = { args.line1, 0 },
+        ["end"] = { args.line2, end_line:len() },
+      }
+    end
+    require("conform").format { async = true, lsp_fallback = true, range = range }
+  end, { range = true }),
 
-	vim.keymap.set({ "n", "v" }, "<leader>F", ":Format<CR>", { desc = "Format" }),
+  -- Keymaps
+  vim.keymap.set({ "n", "v" }, "<leader>F", ":Format<CR>", { desc = "Format" }),
 }
--- lua/jb/plugins/configs/rust.lua
+++ lua/jb/plugins/configs/rust.lua
@@ -1,59 +1,41 @@
 return {
+  {
     "mrcjkb/rustaceanvim",
     version = "*",
     dependencies = {
-        "nvim-lua/plenary.nvim",
-        "mfussenegger/nvim-dap",
+      "nvim-lua/plenary.nvim",
+      "mfussenegger/nvim-dap",
     },
     ft = { "rust" },
 
     config = function()
-        vim.g.rustaceanvim = {
-            inlay_hints = {
-            	highlight = "NonText",
+      vim.g.rustaceanvim = {
+        settings = {
+          ["rust-analyzer"] = {
+            imports = {
+              granularity = {
+                group = "module",
+              },
+              prefix = "self",
             },
-            tools = {
-                hover_actions = {
-                	auto_focus = false,
-                },
-                crate_graph = {
-                    backend = "png",
-                },
-                rustc = {
-                    default_edition = '2024'
-                },
+            procMacro = {
+              enable = false,
+              ignored = {
+                ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+              },
             },
-            settings = {
-                ["rust-analyzer"] = {
-                    imports = {
-                        granularity = {
-                            group = "module",
-                        },
-                        prefix = "self",
-                    },
-                    cargo = {
-                        allfeatures = true,
-                        loadOutDirsFromCheck = true,
-                        runBuildScripts = true,
-                    },
-                    checkOnSave = { -- Add clippy lints for Rust
-                        allfeatures = true,
-                        command = "clippy",
-                        extraArgs = { "--no-deps" },
-                    },
-                    procMacro = {
-                        enable = false,
-                        ignored = {
-                            ["async-trait"] = { "async_trait" },
-                            ["napi-derive"] = { "napi" },
-                            ["async-recursion"] = { "async_recursion" },
-                        },
-                    },
-                },
-            },
-            lens = {
-                enable = true,
-            },
-        }
+          },
+        },
+      }
     end,
+  },
+  {
+    "saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
+    config = function()
+      require("crates").setup {}
+    end,
+  },
 }
